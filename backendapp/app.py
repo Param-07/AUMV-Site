0bf25333a0e31c30401221c1efa464bb7bb4a426
Parmanand <thearyans119@gmail.com>
2026-01-10 19:24:13 +0530

    Fixing app

--- backendapp/app.py
+++ backendapp/app.py
@@ -42,6 +42,8 @@ def create_app(config_object=Config):
 
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
     app = create_app()
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8000)), debug=app.config['DEBUG'])
